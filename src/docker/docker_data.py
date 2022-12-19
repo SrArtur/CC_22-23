5ed2a533e53832beef5f8f36d0b105e1343dc7ff
Arturo Ortiz <aoa2eso@gmail.com>
2022-12-19 16:57:34 +0100

    Version de Python corregida y uso de github secrets. closes #42 closes #43

--- src/docker/docker_data.py
+++ src/docker/docker_data.py
@@ -1,6 +1,7 @@
 import requests
-from src.docker.config import ID_PENINSULA, URL, HEADERS
+# from src.docker.config import ID_PENINSULA, URL, HEADERS
 import datetime
+import os
 
 
 def today_day():
@@ -46,26 +47,29 @@ def get_today_prices(db_format: bool = True, simplify: bool = False):
     :param db_format: Si se desea el tipado de los datos para almacenar en la base de datos.
     :return: prices: precios del día.
     """
-
-    response = requests.get(url=URL, headers=HEADERS)
+    API_KEY = os.environ["API_KEY"]
+    response = requests.get(url='https://api.esios.ree.es/indicators/1001', headers={
+        'Accept': 'application/json; application/vnd.esios-api-v2+json', 'Content-Type': 'application/json',
+        'Host': 'api.esios.ree.es',
+        'Authorization': API_KEY})
     if response.status_code == 200:
         data = response.json()
-        prices = [(parse_date(x['datetime']), convert_to_kwh(x['value'])) for x in
-                  data['indicator']['values'] if
-                  x['geo_id'] == ID_PENINSULA]
+    prices = [(parse_date(x['datetime']), convert_to_kwh(x['value'])) for x in
+              data['indicator']['values'] if
+              x['geo_id'] == 8741]
 
-        if db_format:
-            res = dict()
-            hours = dict()
-            for p in prices:
-                year = str(p[0]['year'])
-                month = str(p[0]['month'])
-                day = str(p[0]['day'])
-                hour = str(p[0]['hour'])
-                price = p[1]
-                hours[hour] = price
-            res[day + month + year] = hours
-            prices = hours if simplify else res
+    if db_format:
+        res = dict()
+    hours = dict()
+    for p in prices:
+        year = str(p[0]['year'])
+    month = str(p[0]['month'])
+    day = str(p[0]['day'])
+    hour = str(p[0]['hour'])
+    price = p[1]
+    hours[hour] = price
+    res[day + month + year] = hours
+    prices = hours if simplify else res
     return prices
 
 
